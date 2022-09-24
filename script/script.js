489c4f63184c13c58457fc964f54daa28020d2ef
Brian Ribeiro <99487199+brianribeiro@users.noreply.github.com>
2022-09-24 12:00:19 -0300

    Iniciando a responsividade

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function drop_menu() {
+    var nav = document.getElementById("nav_menu").querySelectorAll(".nav_options")
+    var drop
+    if (nav[0].style.display === "none") {
+        for (drop = 0; drop < nav.length; drop++) {
+            nav[drop].style.display = "inline-block"
+        }
+    } else {
+        for (drop = 0; drop < nav.length; drop++) {
+            nav[drop].style.display = "none"
+        }
+    }
+}
