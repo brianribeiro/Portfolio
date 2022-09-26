d913bd252aad641e936413966999c6ec17c74217
Brian Ribeiro <99487199+brianribeiro@users.noreply.github.com>
2022-09-26 20:52:20 -0300

    Adicionando scripts

--- script/script.js
+++ script/script.js
@@ -1,8 +1,28 @@
-drop_menu()
+var screen_guardian = setInterval(fix_menu, 100)
 
+function fix_menu() {
+    var screen = document.body.clientWidth
+    var nav = document.getElementById("nav_menu").querySelectorAll(".nav_options")
+    var drop
+
+    if (screen > 1080) {
+        for (drop = 0; drop <nav.length; drop++) {
+            nav[drop].style.display = "inline-block"
+        }
+    } else {
+        if (nav[0].style.display === "inline-block") {
+            for (drop = 0; drop < nav.length; drop++) {
+                nav[drop].style.display = "none"
+            }
+        }
+    }
+}
+
+drop_menu()
 function drop_menu() {
     var nav = document.getElementById("nav_menu").querySelectorAll(".nav_options")
     var drop
+
     if (nav[0].style.display === "none") {
         for (drop = 0; drop < nav.length; drop++) {
             nav[drop].style.display = "block"
